393f8ae4ce09da22f0c511db6516d7e25d44c1e9
Sangbeom Heo <mansaout@gmail.com>
2023-02-02 21:57:46 +0900

    사용자 정보 조회 API 추가

--- models/member/member.model.ts
+++ models/member/member.model.ts
@@ -45,8 +45,19 @@ async function add({
   }
 }
 
+async function findByScreenName(screenName: string): Promise<AuthUser | null> {
+  const memberRef = FirebaseAdmin.getInstance().Firestore.collection(SCR_NAME_COL).doc(screenName);
+  const memberDoc = await memberRef.get();
+  if (memberDoc.exists === false) {
+    return null;
+  }
+  const data = memberDoc.data() as AuthUser;
+  return data;
+}
+
 const MemberModel = {
   add,
+  findByScreenName,
 };
 
 export default MemberModel;
